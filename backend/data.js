d1b44f14504f9a54eab12002546ac0b89feb1c92
Saniya Mulla <shaikhsaniya701@gmail.com>
2022-07-28 16:02:22 +0530

    Second commit

--- backend/data.js
+++ backend/data.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 const data = {
   users: [
     {
-      name: 'Basir',
+      name: 'Saniya',
       email: 'admin@example.com',
       password: bcrypt.hashSync('1234', 8),
       isAdmin: true,
@@ -17,7 +17,7 @@ const data = {
       },
     },
     {
-      name: 'John',
+      name: 'Tasmiya',
       email: 'user@example.com',
       password: bcrypt.hashSync('1234', 8),
       isAdmin: false,
